2706c5666fd16b67aa74cd4eb3975194ecaebcf1
krmsan <sankrmsan@gmail.com>
2022-12-26 23:58:59 +0300

    for ile

--- sorular_1/RakamlatToplamiFor.java
+++ sorular_1/RakamlatToplamiFor.java
@@ -1,4 +1,18 @@
 package sorular_1;
 
 public class RakamlatToplamiFor {
+    public static void main(String[] args) {
+        int sayi = 1234567;
+        int toplam = rakamlariTopla(sayi);
+    }
+
+    private static int rakamlariTopla(int sayi) {
+        int toparr = 0;
+        int top1 = 0;
+        int top = 0;
+
+
+        return top;
+    }
+
 }
